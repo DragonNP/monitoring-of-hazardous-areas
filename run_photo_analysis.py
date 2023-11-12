26a5cd0ae33953890a0b1c6cad4d693d73171c33
Nikita <dragonnp@ya.ru>
2023-11-12 10:34:14 +0300

    Добавлено тестированиие программы

--- run_photo_analysis.py
+++ run_photo_analysis.py
@@ -1,6 +1,6 @@
 import os
 import shutil
-
+import csv
 import cv2
 import numpy as np
 from ultralytics import YOLO
@@ -28,12 +28,13 @@ def draw_human_on_image(path_to_image, coordinates_zona, destination_path):
     cv2.destroyAllWindows()
 
 
-def get_percent_intersection(coordinates, zona):
-    for human in coordinates:
+def get_percent_intersection(coordinates, zones):
+    maxi_result = 0
+    for current_zone in zones:
         # Координаты прямоугольника (верхний левый угол и нижний правый угол)
-        rect_coords = human
+        rect_coords = coordinates
         # Координаты многоугольника
-        poly_coords = zona
+        poly_coords = current_zone
 
         # Создание прямоугольника и многоугольника
         rectangle = box(*rect_coords)
@@ -49,7 +50,9 @@ def get_percent_intersection(coordinates, zona):
         # Процент пересечения
         percent_intersection = (area_intersection / area_rectangle) * 100
 
-        return percent_intersection
+        maxi_result = max(maxi_result, percent_intersection)
+
+    return maxi_result
 
 
 def get_danger_zones(path_to_data_set):
@@ -57,17 +60,53 @@ def get_danger_zones(path_to_data_set):
     for file_name in os.listdir(path_to_data_set):
         if file_name.endswith('.txt'):
             text = open(path_to_data_set + '/' + file_name).read()
-            name_zona = file_name.split('danger_')[1][:-4]
+
+            name_zona = file_name.split('danger_')[1][:-4].split('_')[0]
+            if not (name_zona in result.keys()):
+                result[name_zona] = []
 
             cleaned_text = text.replace("[", "").replace('\n', ' ')
             pairs = cleaned_text.split("], ")
 
             # Преобразовываем строки в целые числа и формируем массив
-            result[name_zona] = [list(map(int, pair.replace(']', '').split(', '))) for pair in pairs]
+            result[name_zona].append([list(map(int, pair.replace(']', '').split(', '))) for pair in pairs])
     return result
 
 
-def main():
+def analysis_folder():
+    print('Загружаются координаты опасных зон... [1/1]')
+    zones = get_danger_zones(PATH_TO_DANGERS_ZONES)
+    print('Загружается обученная модель... [1/1]')
+    model = YOLO(PATH_TO_MODEL)
+    print('-------------------')
+
+    path_to_folder = input('Введите путь к папке с камерами: ')
+    print('-------------------')
+
+    with open('result.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        field = ["camera_name", "frame_filename", "in_danger_zone", 'percent']
+        writer.writerow(field)
+
+        i = 0
+        for camera_name in os.listdir(path_to_folder):
+            print(f'Прогесс [{i+1}/{len(os.listdir(path_to_folder))}]')
+            selected_zona = zones[camera_name]
+            for filename in os.listdir(os.path.join(path_to_folder, camera_name)):
+                coordinates = predict_model.get_coordinates_humans(model,
+                                                                   os.path.join(path_to_folder, camera_name, filename))
+
+                for human in coordinates:
+                    percent_intersection = get_percent_intersection(human, selected_zona)
+                    if percent_intersection >= 15:
+                        res = 'True'
+                    else:
+                        res = 'False'
+                    writer.writerow([camera_name, filename, res, str(percent_intersection / 100)])
+            i += 1
+
+
+def analysis_one_photo():
     print('Загружаются координаты опасных зон... [1/1]')
     zones = get_danger_zones(PATH_TO_DANGERS_ZONES)
     print('Загружается обученная модель... [1/1]')
@@ -106,6 +145,14 @@ def main():
                         './result.jpg')
 
     shutil.rmtree('./runs/detect/predict/')
+
+
+def main():
+    mode = input('Выберите режим: 0 - проанализировать тестовые данные, 1 - проанализировать конкретную фотографию: ')
+    if mode == '0':
+        analysis_folder()
+    elif mode == '1':
+        analysis_one_photo()
 
 
 if __name__ == '__main__':
